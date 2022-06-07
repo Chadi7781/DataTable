b704986d96e8835f8f21ada267ecb59500d50c6e
Chadi Troudi <43905999+Chadi7781@users.noreply.github.com>
2022-06-07 16:48:36 +0100

    add padination and filter

--- src/DataList.js
+++ src/DataList.js
@@ -3,7 +3,11 @@ import React, { useState, useEffect } from "react";
 import BootstrapTable from "react-bootstrap-table-next";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-bootstrap-table-next/dist/react-bootstrap-table2.css";
+import paginationFactory from "react-bootstrap-table2-paginator";
 
+import "react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css";
+import filterFactory, { textFilter } from "react-bootstrap-table2-filter";
+import "react-bootstrap-table2-filter/dist/react-bootstrap-table2-filter.min.css";
 const DataList = () => {
   const [userList, setUserList] = useState([]);
 
@@ -15,19 +19,42 @@ const DataList = () => {
     {
       dataField: "name",
       text: "Name",
-      sort: true
+      sort: true,
+      filter: textFilter()
     },
     {
       dataField: "username",
       text: "Username",
-      sort: true
+      sort: true,
+      filter: textFilter()
     },
     {
       dataField: "email",
       text: "Email",
-      sort: true
+      sort: true,
+      filter: textFilter()
     }
   ];
+
+  const pagination = paginationFactory({
+    page: 1,
+    sizePerPage: 5,
+    lastPageText: ">>",
+    firstPageText: "<<",
+    nextPageText: ">",
+    prePageText: "<",
+    showTotal: true,
+    alwaysShowAllBtns: true,
+    onPageChange: function (page, sizePerPage) {
+      console.log("page", page);
+      console.log("sizePerPage", sizePerPage);
+    },
+    onSizePerPageChange: function (page, sizePerPage) {
+      console.log("page", page);
+      console.log("sizePerPage", sizePerPage);
+    }
+  });
+
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
@@ -41,6 +68,8 @@ const DataList = () => {
         keyField="id"
         columns={columns}
         data={userList}
+        pagination={pagination}
+        filter={filterFactory()}
       />
       {/* <table>
         <tr>
